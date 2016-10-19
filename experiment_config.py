926ac7beee6c80ff3692aaa893c6764f4628f802
Wojciech Kopec <wojciech.j.kopec@gmail.com>
2016-10-19 22:55:24 +0200

    Refactors to ease console running

---
+++ experiment_config.py
@@ -0,0 +1,23 @@
+__author__ = 'wojtek'
+from sources.multi_q_estimator import MultiQEstimator
+from sources.q_estimator import QEstimator
+from experiments_runner import ExperimentsRunner
+
+
+class ExperimentConfig:
+    def __init__(self, epochs=20, learning_steps_per_epoch=2000, test_episodes_per_epoch=100
+                 , frame_repeat=12, resolution=(30, 45), config_file_path="./config/basic.cfg", playAgent=False):
+        self.epochs = epochs
+        self.learning_steps_per_epoch = learning_steps_per_epoch
+        self.test_episodes_per_epoch = test_episodes_per_epoch
+        self.frame_repeat = frame_repeat
+        self.resolution = resolution
+        self.config_file_path = config_file_path
+        self.playAgent = playAgent
+
+
+exampleAgent = lambda actions, config: QEstimator(len(actions), config.resolution)
+bdqnAgentK5p09 = lambda actions, config: MultiQEstimator(len(actions), config.resolution, 5, 0.9, False)
+
+ExperimentsRunner(ExperimentConfig(), bdqnAgentK5p09).run()
+
